585e91d05aa72a0173c0a5ccc725c90464a9bebc
Schevev Aleksey <alcodevil@gmail.com>
2023-04-11 15:26:00 +0300

    added file extension randomization

--- DirectoryCreatev2.py
+++ DirectoryCreatev2.py
@@ -1,10 +1,13 @@
 import os
+import random
 from pathlib import Path
 
 def GenerateFilesForPaths(paths):
     file = open("directory dict.txt", "w")
+    fileExtensions = []
 
-    isCreateFile = input("Добавить пути до файлов в созданые директории, по умолчанию будут созданы файлы с расширением .txt (Y/n)\n")
+    isCreateFile = input("Добавить пути до файлов в созданые директории? (Y/n)\n")
+    
 
     while isCreateFile != "Y" and isCreateFile != "y" and isCreateFile != "N" and isCreateFile != "n":
         isCreateFile = input("Охидаемый ответ Y/n\n")
@@ -19,16 +22,33 @@ def GenerateFilesForPaths(paths):
             else:
                 break
 
+        print( "Выберите расширения для файлов,\n в случае выбора более одного расширения расширения будут распределены случайно,\n по умолчанию будут созданы файлы с расширением .txt\n")
+        while True:
+            tempImput = input('Для выхода из режима ввода нажмите Esc: ')
+            if tempImput == "\x1b":
+                break
+            elif tempImput == "":
+                print("пустая строка не будет использована.\n")
+                break
+            else:
+                fileExtensions.append(tempImput)
+                continue 
+
         for path in paths:
+            random.seed(version=2)
             i = 0
             while i < howMutch:
-                file.write(path+ "/" + "GeneratedTestFile" + str(i) + ".txt" + "\n")
-                i += 1
+                if len(fileExtensions) > 1:
+                    file.write(path+ "/" + "GeneratedTestFile" + str(i) + fileExtensions[random.randint(0, (len(fileExtensions) - 1))] + "\n")
+                    i += 1
+                else:
+                    file.write(path+ "/" + "GeneratedTestFile" + str(i) + ".txt" + "\n")
+                    i += 1
 
 
     else:
         for path in paths:
-            file.write(path + '\n')    
+            file.write(path + '\n')
 
 # получение аргументов длинны и ширины теребуемого дерева порядок важен сначала ширина потом длина, разделитель пробел 
 inputParams = input("введи ширину и глуюину предполагаемого дерева через пробел\n")
